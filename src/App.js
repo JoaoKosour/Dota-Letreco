b16fd467aedc460cd46938193fb93e0cfbb1853f
joao <joaokosour@gmail.com>
2022-05-16 00:36:18 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./Components/Footer/Footer.js";
+import Header from "./Components/Header/Header.js";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <div className='App'></div>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
