88129685577580cd27348b05396ef150e031a0be
joao <joaokosour@gmail.com>
2022-05-19 01:04:20 -0300

    added router

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
 import './Header.css';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="Header">
-        <a href="">D</a><a href=""></a><a href=""></a>
+      <Link to="/Dotreko">Dotreko</Link>
+      <Link to="/Letrekomon">Letrekomon</Link>
+      <Link to="/"></Link>
+      <Link to="/Login">Login</Link>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Route path="/" element={<App />} />
+    <Route path="expenses" element={<Expenses />} />
+    <Route path="invoices" element={<Invoices />} />
+  </BrowserRouter>
 );
-
